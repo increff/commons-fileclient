ca615a1e81dde51fcd17cd66bb9a7f25ae9fb778
abhaychandna <email@example.com>
2024-02-08 15:51:46 +0530

    get, getSignedUri added

--- src/main/java/com/increff/commons/fileclient/AbstractFileProvider.java
+++ src/main/java/com/increff/commons/fileclient/AbstractFileProvider.java
@@ -15,6 +15,7 @@
 package com.increff.commons.fileclient;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +24,7 @@ public abstract class AbstractFileProvider {
 	/* API to use when directly writing */
 	public abstract void create(String filePath, InputStream is) throws FileClientException;
 
-	public void create(String filePath, InputStream is, Map<String,String> metaData) throws FileClientException {
+	public void create(String filePath, InputStream is, Map<String, String> metaData) throws FileClientException {
 		throw new FileClientException("This method is not implemented");
 	}
 
@@ -50,4 +51,8 @@ public abstract class AbstractFileProvider {
 
 	/* API to use when directly reading */
 	public abstract InputStream get(String filePath) throws FileClientException;
+
+	public URL getSignedUri(String filePath) throws FileClientException {
+		throw new FileClientException("Method not implemented");
+	}
 }
